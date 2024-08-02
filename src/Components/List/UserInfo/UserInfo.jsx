a54dcfa9eca3600fa4dab164b7d5f8b9bbe7eea6
Konain Raza <131591948+Konain-Raza@users.noreply.github.com>
2024-08-02 09:55:23 +0500

    Update UserInfo.jsx

--- src/Components/List/UserInfo/UserInfo.jsx
+++ src/Components/List/UserInfo/UserInfo.jsx
@@ -1,30 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./userinfo.css";
 import avatar from "./Images/avatar.png";
 import Settings from "../../Settings/Settings";
 import Cookies from "universal-cookie";
-import { signOut } from "firebase/auth"; // Import the signOut function
+import { signOut } from "firebase/auth";
 import { auth } from "../../firebase-config";
 
 const UserInfo = ({ currentUser }) => {
   const [settingsVisible, setSettingsVisible] = useState(false);
-  const [chatUser, setChatUser] = useState(currentUser); // Initialize chatUser state with currentUser
+  const [chatUser, setChatUser] = useState(currentUser);
   const cookies = new Cookies();
 
-  // Function to toggle settings visibility
+  useEffect(() => {
+    setChatUser(currentUser);
+  }, [currentUser]);
+
   const toggleSettings = () => {
     setSettingsVisible((prev) => !prev);
   };
 
-  // Callback function to update chatUser state
   const updateChatUser = (newUserData) => {
     setChatUser(newUserData);
   };
 
-  // Function to handle user sign-out
   const handleLogout = async () => {
     try {
-      await signOut(auth); // Assuming `auth` is your Firebase authentication instance
+      await signOut(auth);
       cookies.remove("auth-token");
       window.location.reload();
     } catch (error) {
@@ -36,8 +37,8 @@ const UserInfo = ({ currentUser }) => {
     <div id="userinfo">
       <div className="user">
         <div id="name">
-          <img src={chatUser.avatar || avatar} alt="user-image" />
-          <h2>{chatUser.username}</h2>
+          <img src={chatUser?.avatar || avatar} alt="user-image" />
+          <h2>{chatUser?.username || "Guest"}</h2>
         </div>
         <div id="icons-box">
           <i className="ri-logout-box-r-line" onClick={handleLogout}></i>
